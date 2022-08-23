eb70fbb5b7e45bfbe388e716551896d95140cafe
Ahmed Bargady <bargadyahmed2000@gmail.com>
2022-08-23 01:46:32 +0100

    removing allow_credintials

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ from breast_cancer_prediction.classes.main import BreastCancerPrediction
 
 app = FastAPI()
 
-origins = ["http://localhost:5173","http://127.0.0.1:5173","https://jina-ml.vercel.app","https://ml.ahmedbargady.me"]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["GET","POST"],
     allow_headers=["*"]
 )
